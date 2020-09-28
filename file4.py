8c98e971186e81deb2ed41ef4075e35ba77ae0a9
sdwivedi05 <64338534+sdwivedi05@users.noreply.github.com>
2020-09-28 12:59:46 -0700

    Create file4.py

---
+++ file4.py
@@ -0,0 +1,8 @@
+print("Devops Certification")
+lst1=[2,6,90]
+for x in lst1:
+    if x%2!=0:
+        print("List contains an odd numver", x)
+        break
+else:
+    print("List does not conatin an odd number")
